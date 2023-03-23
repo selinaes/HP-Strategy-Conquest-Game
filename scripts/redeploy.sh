d4e981bf37f879a44c2326560cc5c106eb76b953
yxfeng <yxfeng@umich.edu>
2023-03-23 01:04:01 -0400

    update deploy script

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,5 +5,5 @@ docker kill citest-651
 docker rm citest-651
 
 # now run the new one
-docker run -d --name citest-651 -p 1651:1651 -t citest ./gradlew run
+docker run -d --name citest-651 -p 1651:1651 -t citest ./gradlew run-server
 
