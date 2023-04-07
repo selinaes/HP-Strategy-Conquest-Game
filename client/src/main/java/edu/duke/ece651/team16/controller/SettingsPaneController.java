729a6a0e868541820d6080beb248a8390eaa5bfa
yxfeng <yxfeng@umich.edu>
2023-04-07 19:52:31 -0400

    test

---
+++ client/src/main/java/edu/duke/ece651/team16/controller/SettingsPaneController.java
@@ -0,0 +1,32 @@
+package edu.duke.ece651.team16.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+
+public class SettingsPaneController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button toggleMusicButton;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private void handleExit() {
+        // Implement exit button functionality
+    }
+
+    @FXML
+    private void handleMusicToggle() {
+        // Implement music toggle button functionality
+    }
+
+    @FXML
+    private void handleVolumeChange() {
+        // Implement volume slider functionality
+    }
+}
