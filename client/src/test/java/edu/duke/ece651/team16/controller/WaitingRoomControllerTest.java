44096db49b9dfeb8156024622a23c4bb86c6d50e
YUME-FF <zf70@duke.edu>
2023-04-25 23:50:44 -0400

    StartGame JoinRoom WaitingRoom Test

---
+++ client/src/test/java/edu/duke/ece651/team16/controller/WaitingRoomControllerTest.java
@@ -0,0 +1,88 @@
+package edu.duke.ece651.team16.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.testfx.util.WaitForAsyncUtils.waitForFxEvents;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.net.Socket;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class WaitingRoomControllerTest extends ApplicationTest {
+  private WaitingRoomController waitingRoomController;
+  private Client client;
+
+  private Socket makeSocket() throws IOException {
+    Socket mockSocket = Mockito.mock(Socket.class);
+    InputStream mockInputStream = Mockito.mock(InputStream.class);
+    OutputStream mockOutputStream = Mockito.mock(OutputStream.class);
+    Mockito.when(mockSocket.getInputStream()).thenReturn(mockInputStream);
+    Mockito.when(mockSocket.getOutputStream()).thenReturn(mockOutputStream);
+    return mockSocket;
+  }
+
+  private BufferedReader makeMockSocketReader(Socket mockSocket) throws IOException {
+    return new BufferedReader(new InputStreamReader(mockSocket.getInputStream()));
+  }
+
+  private PrintWriter makeMockSocketSend(Socket mockSocket) throws IOException {
+    return new PrintWriter(mockSocket.getOutputStream(), true);
+  }
+
+  private PrintStream makeOut() {
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(byteArrayOutputStream);
+    return out;
+  }
+
+  private void setClient() throws IOException, Exception {
+    Socket mSocket = makeSocket();
+    BufferedReader socketReceive = makeMockSocketReader(mSocket);
+    PrintWriter socketSend = makeMockSocketSend(mSocket);
+    BufferedReader inputSource = mock(BufferedReader.class);
+    PrintStream out = makeOut();
+
+    client = new Client(inputSource, out, socketReceive, socketSend);
+    Field socketReceiveField = client.getClass().getDeclaredField("socketReceive");
+    socketReceiveField.setAccessible(true);
+    BufferedReader mockReader = mock(BufferedReader.class);
+
+    when(mockReader.readLine()).thenReturn("stage Complete").thenReturn("map");
+    socketReceiveField.set(client, mockReader);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception, IOException {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/WaitingRoom.fxml"));
+    AnchorPane anchorPane = loader.load();
+    waitingRoomController = loader.getController();
+    setClient();
+    waitingRoomController.setClient(client);
+    Scene scene = new Scene(anchorPane);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Test
+  public void test_next() {
+    waitForFxEvents();
+    clickOn("#nextWaiting");
+  }
+
+}
